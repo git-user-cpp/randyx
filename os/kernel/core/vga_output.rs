96c90db805b171a5cb37912bcd2a857722f179fa
Andrew Kushyk <61907955+git-user-cpp@users.noreply.github.com>
2023-12-03 14:29:47 +0200

    v0.1.0 small fix of VGA buffer (#5)
    * toml file was added for building the kernel
    
    * the target for build was set
    
    * implemented basic output using VGA buffer
    
    * RandyX: launching via qemu
    
    * RandyX: documentation + multikernel architecture
    
    * small fix of the vga buffer fn

--- os/kernel/core/vga_output.rs
+++ os/kernel/core/vga_output.rs
@@ -1,7 +1,7 @@
-pub fn vga_output_message(HELLO: &[u8]) {
+pub fn vga_output_message(message: &[u8]) {
     let vga_buffer = 0xb8000 as *mut u8;
 
-    for(i, &byte) in HELLO.iter().enumerate(){
+    for(i, &byte) in message.iter().enumerate(){
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
