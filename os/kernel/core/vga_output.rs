9cd16c1de3f3f9a48a5464a693994be5d7c4f52f
git-user-cpp <akushyk799@gmail.com>
2023-12-07 14:14:27 +0200

    RandyX v0.1.1: - fixed bufferoverflow issue

--- os/kernel/core/vga_output.rs
+++ os/kernel/core/vga_output.rs
@@ -1,4 +1,11 @@
-pub fn vga_output_message(message: &[u8]) {
+pub fn vga_output_message(_message: &[u8]) {
+    const MAX_LEN: usize = 32;
+    let mut message = _message;
+
+    if message.len() > MAX_LEN {
+        message = b"Too many bytes to output";
+    }
+
     let vga_buffer = 0xb8000 as *mut u8;
 
     for(i, &byte) in message.iter().enumerate(){
